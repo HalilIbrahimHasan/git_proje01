6889c8c0d71f3257ce0aa017d523cf05f96fdea0
Halil Ibrahim <hisetkal@gmail.com>
2020-06-21 15:33:20 -0400

    feature branch ten ilk commit

--- src/package01/FirstClass.java
+++ src/package01/FirstClass.java
@@ -7,6 +7,7 @@ public class FirstClass {
 		System.out.println("Ilk commit from local repository");
 		System.out.println("ikinci commit from local repository");
 
+		System.out.println("Yeni branch olusturdum ev adi feature");
 	}
 
 }
